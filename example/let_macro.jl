6aa7a76bc8ca9e36978d15d5a0e5caa0c288578f
Ted Meyer <tmathmeyer@gmail.com>
2015-08-16 02:29:46 -0400

    cleaning up examples

--- example/let_macro.jl
+++ example/let_macro.jl
@@ -1,7 +1,7 @@
-(#redef (letr ((iname iexpr) (name expr) ...) evalme)
+(# (letr ((iname iexpr) (name expr) ...) evalme)
         ((lambda (iname) (letr ((name expr) ...) evalme)) iexpr))
 
-(#redef (letr ((iname iexpr)) evalme)
+(# (letr ((iname iexpr)) evalme)
         ((lambda (iname) evalme) iexpr))
 
 (letr ((x 5) (y 6) (z 7))
